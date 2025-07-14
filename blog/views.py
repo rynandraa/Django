7099d6d49ae7b8367c2b8969e041a216c65563f0
rynandraa <ryanandrayadi0@gmail.com>
2025-07-14 11:12:13 +0700

    Fix logout bug by using GET request, update menu Profile

--- blog/views.py
+++ blog/views.py
@@ -3,13 +3,13 @@ from .models import Post
 from django.views.generic import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.decorators.cache import never_cache
 
 
 # Create your views here.
 @never_cache
 @login_required
-
 def home(request):
     posts = Post.objects.all()
     return render(request, 'blog/home.html', {"posts" :posts})
@@ -27,17 +27,29 @@ class PostCreateView(CreateView):
     fields = ["title", "content"]
     success_url = reverse_lazy("home")
 
+    def form_valid(self, form):
+        form.instance.author = self.request.user  # ← inilah kuncinya!
+        return super().form_valid(form)
+
 
 #Update Post
-class PostUpdateView(UpdateView):
+class PostUpdateView(UpdateView, LoginRequiredMixin, UserPassesTestMixin):
     model = Post
     template_name = "blog/post_form.html"
     fields = ["title", "content"]
     success_url = reverse_lazy("home")
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author or self.request.user.is_staff
 #
 
 #Delete Post
-class PostDeleteView(DeleteView):
+class PostDeleteView(DeleteView, LoginRequiredMixin, UserPassesTestMixin):
     model = Post
     template_name = "blog/post_delete.html"
     success_url = reverse_lazy("home")
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author or self.request.user.is_staff
--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
+from blog.models import Post
 
 
 # Create your views here.
@@ -21,4 +22,5 @@ def register(request):
 
 @login_required
 def profile(request):
-    return render(request, "users/profile.html")
+    user_posts = Post.objects.filter(author=request.user)
+    return render(request, "users/profile.html", {'posts' : user_posts})
